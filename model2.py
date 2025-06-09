469ea8c93d7e285b604480c32615ea5e7d6ed1ed
Liwia Florkiewicz <s193148@student.pg.edu.pl>
2025-06-10 00:35:26 +0200

    deleted frames and labels from different folders

--- model2.py
+++ model2.py
@@ -3,16 +3,16 @@ from ultralytics import YOLO
 # Load model (YOLOv8s as example)
 model = YOLO('yolov8m.pt')
 
-# Train
-try:
-    model.train(
-        data='dataset/data.yaml',
-        epochs=5,
-        imgsz=640,
-        batch=4
-    )
-except Exception as e:
-    print(f"Błąd podczas treningu: {e}")
+# # Train
+# try:
+#     model.train(
+#         data='dataset/data.yaml',
+#         epochs=5,
+#         imgsz=640,
+#         batch=4
+#     )
+# except Exception as e:
+#     print(f"Błąd podczas treningu: {e}")
 
 # Validate
 model.val(data='dataset/data.yaml')
@@ -20,8 +20,7 @@ model.val(data='dataset/data.yaml')
 # Inference
 results = model('prediction/image.jpg', save=True)
 
-# Print results
-print(results)
-
 # Print summary
-print("Training and validation completed. Results saved in 'prediction/results'.")
+print("Training and validation completed.")
+
+
